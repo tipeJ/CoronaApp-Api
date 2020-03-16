b031d7d0abc98eb584225a67540acffc5aa4caed
tipej <tiitus.jarvinen@gmail.com>
2020-03-16 14:50:45 +0200

    Added a new test source

--- resources/sources.go
+++ resources/sources.go
@@ -1,18 +1,28 @@
 package resources
 
 import (
+	"time"
+
 	"github.com/tipej/CoronaApp-Api/models"
 )
 
 var NewsSources = map[string]models.NewsSource{
 	"NYPost": models.NewsSource{
-		RssUrl: "https://nypost.com/coronavirus/feed/",
+		RssUrl:     "https://nypost.com/coronavirus/feed/",
+		DateFormat: time.RFC1123Z,
 	},
 	"NYTimes": models.NewsSource{
-		RssUrl:  "https://rss.nytimes.com/services/xml/rss/nyt/Health.xml",
-		Keyword: "Coronavirus (2019-nCoV)",
+		RssUrl:     "https://rss.nytimes.com/services/xml/rss/nyt/Health.xml",
+		Keyword:    "Coronavirus (2019-nCoV)",
+		DateFormat: time.RFC1123Z,
 	},
 	"Politico (EU)": models.NewsSource{
-		RssUrl: "https://www.politico.eu/tag/coronavirus/feed/",
+		RssUrl:     "https://www.politico.eu/tag/coronavirus/feed/",
+		DateFormat: time.RFC1123Z,
+	},
+	"BBC (UK)": models.NewsSource{
+		RssUrl:     "http://feeds.bbci.co.uk/news/health/rss.xml",
+		Keyword:    "Coronavirus",
+		DateFormat: time.RFC1123,
 	},
 }
